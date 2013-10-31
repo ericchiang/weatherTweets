39e4d697d6a28dcedbe5ca95f3ff495c3d844711
ericchiang <eric.chiang.m@gmail.com>
2013-10-31 19:54:34 +0100

    Feature extraction improved

--- featextract.py
+++ featextract.py
@@ -8,12 +8,16 @@ __license__   = "GPL"
 __version__   = "3.0"
 
 import csv
+import re
 import string
 
 meta_tags = ['@mention','{link}']
-alpha_numeric_word = ['ALPHA_NUMERIC_WORD']
-numeric_word = ['NUMERIC_WORD']
+alphanumeric_word = 'ALPHANUMERIC_WORD'
+numeric_word = 'NUMERIC_WORD'
 
+re_number = re.compile(r'^\d*\.?\d+$')
+re_contains_num = re.compile('\d')
+punct_no_apost = string.punctuation.replace("'",'')
 
 """
 Parse CSV file into data matrix
@@ -31,16 +35,19 @@ Parse tweet into bag of words
 def parseTweet(tweet):
     bag_of_words = []
     for tag in meta_tags:
-        tweet = tweet.replace(tag,'')
+        tweet = tweet.replace(tag,' ')
+
+    for punch_char in punct_no_apost:
+        tweet = tweet.replace(punch_char,' ')
+
     tweet = tweet.split()
     for i in range(len(tweet)):
         word = tweet[i].lower()
-        while True:
-            last_word = word
-            word = word.strip(string.punctuation)
-            if word == last_word:
-                break
         if word:
+            if re_number.match(word):
+                word = numeric_word
+            elif re_contains_num.match(word):
+                word = alphanumeric_word
             bag_of_words.append(word)
     return bag_of_words
 
