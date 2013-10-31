7c0aebf41c04f24cb8b19f8b0ddb61b1e2db7871
ericchiang <eric.chiang.m@gmail.com>
2013-10-31 19:54:14 +0100

    Cross validation improved

--- crossvalidate.py
+++ crossvalidate.py
@@ -13,10 +13,10 @@ import re
 import math
 import numpy as np
 from sklearn import cross_validation
-from naivebayes import BagOfWordsBayes
+from naivebayesV2 import BagOfWordsBayes
 from utils import printInfo,meanSquaredError,rootMeanSquaredError # utils.py
 from featextract import * # featextract.py
-
+from hmm import HiddenMarkovModel
 
 def usage():
     print """
@@ -26,7 +26,7 @@ def usage():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         usage()
         sys.exit(2)
 
@@ -36,7 +36,13 @@ if __name__ == '__main__':
         sys.stderr.write("[Error] Could not open file '%s'" % (sys.argv[1],))
         sys.exit(14)
 
-    var_names = sys.argv[2:]
+    if len(sys.argv) > 2:
+        var_names = sys.argv[2:]
+    else:
+        var_names = ['s1','s2','s3','s4','s5',
+                     'w1','w2','w3','w4',
+                     'k1','k2','k3','k4','k5','k6','k7','k8',
+                     'k9','k10','k11','k12','k13','k14','k15']
 
     # Parse csv file
     data = parseTwitterCSV(tweet_train)
@@ -48,9 +54,12 @@ if __name__ == '__main__':
     # Parse tweets
     tweet_data = []
     for row in data:
-        tweet_data.append(parseTweet(row[1]))
+        t_data = parseTweet(row[1])
+        tweet_data.append(t_data)
     assert len(tweet_data) == len(data)    
     printInfo("%d tweets parsed" % (len(tweet_data),))
+
+    mses = []
 
     for var_name in var_names:
         # Get index of desired label
@@ -87,13 +96,17 @@ if __name__ == '__main__':
             y_test  = y[test_indices]
     
             progress = (fold_number,num_folds,)
-            naiveBayes = BagOfWordsBayes()
-            naiveBayes.fit(X_train,y_train)
-            fold_y_pred = naiveBayes.predict(X_test)
+            classifier = BagOfWordsBayes()
+            classifier.fit(X_train,y_train)
+            fold_y_pred = classifier.predict(X_test)
             for i in range(len(test_indices)):
                 assert y[test_indices[i]] == y_test[i]
                 y_pred[test_indices[i]] = fold_y_pred[i]
-            #printInfo(" %2s/%s" % (fold_number,num_folds,))
+            printInfo(" %2s/%s" % (fold_number,num_folds,))
             fold_number += 1
         mse = meanSquaredError(y_pred,y)
-        print("%s %s" % (var_name,mse))
+        printInfo("%s MSE: %s" % (var_name,mse))
+        mses.append(mse)
+    totalMSE = sum(mses) / len(mses)
+    printInfo("Total MSE: %s" % (totalMSE,))
+    printInfo("Total RMSE: %s" % (math.sqrt(totalMSE),))
